3998b50f47f1a560f3126b56017bfebc9e6d4fbb
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-02-03 13:21:34 +0100

    Started prepare for adding rspec tests.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'bundler'
+require 'yaml'
+
+Bundler.require :default, :development
+
+SPEC_DIR = File.dirname(__FILE__) unless defined? SPEC_DIR
+MR_CONFIG = YAML.load_file(File.join(SPEC_DIR, 'config.yml')) unless defined? MR_CONFIG
+
+Rspec.configure do |c|
+  #c.fail_fast = true 
+end
+
+Dir["#{SPEC_DIR}/models/*.rb"].each { |f| require f }
+Dir["#{SPEC_DIR}/support/**/*.rb"].each { |f| require f }
---
+++ spec/unit/massive_record_base_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe MassiveRecord::ORM::Base do
+  describe "searchable" do
+    it "should come some tests here" do
+      fail
+    end
+  end
+end
